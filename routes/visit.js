330929b74372109354c588cc4594268cde55de40
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-03 10:46:29 -0700

    user controller on endpoint

--- routes/visit.js
+++ routes/visit.js
@@ -1,5 +1,8 @@
 var express = require("express");
+const visit = require("../controllers/visit");
 var router = express.Router();
+
+router.use("/", visit);
 
 /* GET visits listing. */
 router.get("/", function (req, res, next) {
