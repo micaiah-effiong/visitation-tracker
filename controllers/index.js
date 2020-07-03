fa27c161a67ac5ac7beec2c20d32d3419aac5cbe
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-03 09:46:12 -0700

    make db available to index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
+const db = require("../models/index.js");
 const user = require("./user.js");
 const visit = require("./visit.js");
-module.exports = function (db) {
+module.exports = function () {
 	return {
 		user: user(db),
 		visit: visit(db),
