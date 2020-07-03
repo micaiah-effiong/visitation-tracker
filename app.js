ddfeff27be107cfa4ce987ef2ded4eb4b11912bf
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-03 09:36:25 -0700

    add: setup for passport auth

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+let session = require("express-session");
+let passport = require("passport");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 
 // view engine setup
@@ -18,9 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	session({
+		secret: "secret",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
