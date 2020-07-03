34d2b7c09513bf613ed3fdbf3e0b69fb65a8cfbb
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-03 10:09:52 -0700

    add CRUD for user and visit controller - link both to their routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,52 @@
+const asyncHandler = require("../handlers/async-handler.js");
 module.exports = function (db) {
 	return {
-		getOne(req, res, next) {},
-		getAll(req, res, next) {},
-		create(req, res, next) {},
-		update(req, res, next) {},
-		delete(req, res, next) {},
+		getOne: asyncHandler(async (req, res, next) => {
+			let id = Number(req.params.id);
+			let user = await db.user.findByPk(id);
+			res.json({
+				success: true,
+				data: user.toPublicJSON(),
+			});
+		}),
+
+		getAll: asyncHandler(async (req, res, next) => {
+			let users = await db.user.findAll();
+			let data = users.map((user) => user.toPublicJSON());
+			res.json({
+				success: true,
+				data,
+			});
+		}),
+
+		create: asyncHandler(async function (req, res, next) {
+			let user = await db.user.create(req.body);
+			res.json({
+				success: true,
+				data: user.toPublicJSON(),
+			});
+		}),
+
+		update: asyncHandler(async (req, res, next) => {
+			let id = Number(req.params.id);
+			let user = await db.user.update(req.body, {
+				where: {
+					id,
+				},
+			});
+			res.json({
+				success: true,
+				data: user.toJSON(),
+			});
+		}),
+
+		delete: asyncHandler(async (req, res, next) => {
+			let id = Number(req.params.id);
+			let user = await db.user.destroy(id);
+			res.json({
+				success: true,
+				data: user.toJSON(),
+			});
+		}),
 	};
 };
