546e9787e34e094a0aa9d34272899c7c911e3200
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-03 09:47:09 -0700

    setup user and visit comtrollers

---
+++ controllers/visit.js
@@ -0,0 +1,9 @@
+module.exports = function (db) {
+	return {
+		getOne(req, res, next) {},
+		getAll(req, res, next) {},
+		create(req, res, next) {},
+		update(req, res, next) {},
+		delete(req, res, next) {},
+	};
+};
