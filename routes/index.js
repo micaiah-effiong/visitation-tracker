1af95f29d41fd2f43f84cb89817444d96773165e
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-04 06:05:16 -0700

    add: is auth middleware to routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const isAuth = require("../middlewares/isAuth");
 const user = require("./user.js");
 const visit = require("./visit.js");
+const auth = require("./auth.js");
 
 router.use("/users", user);
-router.use("/visit", visit);
+router.use("/auth", auth);
+router.use(isAuth);
+router.use("/visits", visit);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
