aca2ee948110cf6adce601a2dd9961d1a57e099e
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-04 05:58:37 -0700

    Refactor: replace fields(firstname, lastname) with name. \n Add: hooks verify password instance method and toPublicJSON

--- models/user.js
+++ models/user.js
@@ -1,27 +1,60 @@
+const bcrypt = require("bcrypt");
+const _ = require("underscore");
+
 module.exports = function (sequelize, DataType) {
-	let user = sequelize.define("user", {
-		firstname: {
-			type: DataType.STRING,
-			validate: {
-				len: [3],
+	let model = sequelize.define(
+		"user",
+		{
+			name: {
+				type: DataType.STRING,
+				validate: {
+					len: [3],
+				},
+			},
+			email: {
+				type: DataType.STRING,
+				unique: true,
+				validate: {
+					isEmail: true,
+				},
+			},
+			phone: { type: DataType.INTEGER(11).ZEROFILL },
+			type: {
+				type: DataType.ENUM,
+				allowNull: false,
+				values: ["student", "staff", "visitors", "admin"],
 			},
 		},
-		lastname: {
-			type: DataType.STRING,
-			validate: {
-				len: [3],
+		{
+			hooks: {
+				beforeValidate: function (user, options) {
+					// convert email to lower case and trim
+					if (user.email) {
+						user.email = user.email.toLowerCase().trim();
+					}
+				},
 			},
-		},
-		email: {
-			type: DataType.STRING,
-			validate: {
-				isEmail: true,
-			},
-		},
-		phone: { type: DataType.INTEGER(11).ZEROFILL },
-		type: {
-			type: DataType.ENUM,
-			values: ["student", "staff", "visitors", "admin"],
-		},
-	});
+		}
+	);
+
+	/*
+	 * verify user password using bcrypt compare
+	 * @param {String} password the user password
+	 * @return {Boolean} <Promise> true if password matches hash
+	 */
+	model.prototype.verifyPassword = async function (password) {
+		let adminInfo = await this.getUserAdminAccessInfo();
+		try {
+			return await bcrypt.compare(password, adminInfo.hash);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	model.prototype.toPublicJSON = function () {
+		// ommit some fields
+		return _.omit(this.toJSON(), "salt", "hash", "password");
+	};
+
+	return model;
 };
