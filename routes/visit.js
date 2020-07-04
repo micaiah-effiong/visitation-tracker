5d0891712092ded0cc0ad1d099968dfef2a7af67
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-04 06:07:48 -0700

    set up route controllers

--- routes/visit.js
+++ routes/visit.js
@@ -1,12 +1,19 @@
 var express = require("express");
-const visit = require("../controllers/visit");
+const {
+	getOne,
+	getAll,
+	create,
+	update,
+	remove,
+} = require("../controllers/index").visit;
 var router = express.Router();
-
-router.use("/", visit);
 
 /* GET visits listing. */
 router.get("/", function (req, res, next) {
 	res.send("respond with a resource");
 });
+
+router.route("/").post(create).get(getAll);
+router.route("/:id").get(getOne).put(update).delete(remove);
 
 module.exports = router;
