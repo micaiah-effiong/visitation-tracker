abd9ad9838d0fd5b194d759bb0a92fa97f7fb694
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-05 07:57:38 -0700

    remove default handler for visit root endpont

--- routes/visit.js
+++ routes/visit.js
@@ -9,9 +9,9 @@ const {
 var router = express.Router();
 
 /* GET visits listing. */
-router.get("/", function (req, res, next) {
-	res.send("respond with a resource");
-});
+// router.get("/", function (req, res, next) {
+// 	res.send("respond with a resource");
+// });
 
 router.route("/").post(create).get(getAll);
 router.route("/:id").get(getOne).put(update).delete(remove);
