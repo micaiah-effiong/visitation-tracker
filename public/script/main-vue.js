9c1054c93d5bbe25283753bed53b5c30a82c9abe
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-08 06:18:28 -0700

    added fields to get needed functionality

--- public/script/main-vue.js
+++ public/script/main-vue.js
@@ -2,21 +2,56 @@ new Vue({
   el: "#app",
   data: {
     search: "",
+    search2: "",
     searchResult: [],
-    visitor: "",
-    person: "",
+    searchResult2: [],
+    visitForm: {
+      directedTo: "",
+      email: "",
+      purpose: "",
+    },
+    result: "",
   },
   // created: {},
   methods: {
-    searchInput: function () {
-      fetch("/users?select=id,name,email&name[like]=" + this.search)
+    searchInput: function (searchBox) {
+      console.log(searchBox);
+
+      fetch("/users?select=id,name,email&name[like]=" + this[searchBox])
         .then((res) => res.json())
         .then((res) => {
-          console.log(res.data);
-          this.searchResult = res.data;
+          searchBox === "search"
+            ? (this.searchResult = res.data)
+            : (this.searchResult2 = res.data);
         })
         .catch(console.log);
     },
-    setInput: function (value, target) {},
+    submitVisit: function () {
+      fetch("/visits", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.visitForm),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res.data);
+          this.result = "done";
+          return res;
+        })
+        .catch((err) => {
+          this.return = "An error occurred";
+          console.error(error, "An error occurred");
+        });
+    },
+    setInput: function (value, target) {
+      if (value) {
+        this.visitForm.email = value;
+      }
+      if (target) {
+        this.visitForm.directedTo = target;
+      }
+    },
   },
 });
