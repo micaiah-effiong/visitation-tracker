a68acc4785d987f5a7df92dbabeafdb45282892f
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-09 06:54:53 -0700

    Added error-response if user submits a form without user type for registration

--- handlers/index.js
+++ handlers/index.js
@@ -1,13 +1,14 @@
 const qureyHandler = require("./advanceQuery");
 const queryToSequelize = require("./queryToSequelize");
 const pagination = require("./pagination");
+const errorResponse = require("./error-response");
 
 module.exports = {
   /*
    * @param {String} str
    * return string with first letter as capital
    * convert str to sentence case
-   * toSentenceCase("foo") => "Foo"
+   * toSentenceCase("foo") // "Foo"
    */
   toSentenceCase(str) {
     // to Sentence Case function
@@ -15,6 +16,7 @@ module.exports = {
       str.toLowerCase().split("")[0].toUpperCase() + str.substr(1).toLowerCase()
     );
   },
+  errorResponse,
   qureyHandler,
   queryToSequelize,
   pagination,
